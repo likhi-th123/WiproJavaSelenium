a181a4fb9de1d712990e19485fbe69b552c00292
Likhith Kumar V <146958961+likhi-th123@users.noreply.github.com>
2025-08-14 06:35:43 +0530

    Add files via upload

---
+++ 21-07-2025/Variables.java
@@ -0,0 +1,35 @@
+public class Variables {
+    public static void main(String[] args){
+
+        //String variables
+        String name = "John";
+        String country = "USA";
+        System.out.println(name);
+        System.out.println(country);
+
+        //Integer variables
+        int mynum = 28;
+        System.out.println(mynum);
+
+        //Floating Point variables
+        float price = 25.5f;
+        System.out.println(price);
+
+        //Char variable
+        char letter = 'P';
+        System.out.println(letter);
+
+        //Boolean variable
+        boolean fact = true;
+        System.out.println(fact);
+
+        //Writing multiple variables
+        int a = 10, b = 20, c = 30;
+        System.out.println(a+b+c);
+
+        int x,y,z;
+        x = y = z = 10;
+        System.out.println(x+y+z);
+
+    }
+}
---
+++ 21-07-2025/Welcome.java
@@ -0,0 +1,10 @@
+public class Welcome {
+    public static void main( String[] args) {
+
+        // Basic Hello World code
+
+        System.out.println("Hello World!");
+        System.out.println("Welcome to Java development.");
+
+    }
+}
