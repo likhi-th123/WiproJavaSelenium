8b685e16614bfaa9f719dce75cc804659be7491a
Likhith Kumar V <146958961+likhi-th123@users.noreply.github.com>
2025-08-14 07:24:14 +0530

    Delete Task directory

--- Task/src/project/CourseNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package project;
-
-//Custom Exceptions
-
-class CourseNotFoundException extends Exception {
-    public CourseNotFoundException(String message) {
-        super(message);
-    }
-}
--- Task/src/project/Registrable.java
+++
@@ -1,7 +0,0 @@
-package project;
-
-public interface Registrable {
-	 void registerCourse(Course course) throws CourseAlreadyRegisteredException;
-	 void dropCourse(String courseId) throws CourseNotFoundException;
-
-}
